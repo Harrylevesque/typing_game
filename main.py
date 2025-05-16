5096445870b0f359e50153e22e51dbc650fce525
Harrylevesque <harrylevesque17@gmail.com>
2025-05-15 20:33:31 -0400

    created initial concept and setup basic libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import time
+import sys
+import os
+import colorama
+import readchar
+
+user_name = input("Enter your name: ")
+print(f"Hello, {user_name}! this is a simple text-based game where you will type the the recepies. this game's goal is to help you type faster.")
+
+
